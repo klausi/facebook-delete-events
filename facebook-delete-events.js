a7ee4d836e0718a44f3ceee5681829c200043c53
Klaus Purer <klaus.purer@gmail.com>
2017-01-27 17:18:37 +0100

    Added meta info to script

--- facebook-delete-events.js
+++ facebook-delete-events.js
@@ -2,6 +2,9 @@
 // @name        Facebook delete past events
 // @namespace   klausi
 // @description Deletes all your past events on Facebook
+// @license     GPLv2; https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+// @homepageURL https://github.com/klausi/facebook-delete-events
+// @author      klausi
 // @include     https://www.facebook.com/events/past
 // @version     1
 // @grant       none
